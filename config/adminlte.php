0ef20accc61624847a8696e389ff268ad93cfb66
Cleison Freitas <cleison.ferreira@aldesenvolvimento.com.br>
2022-12-06 16:54:48 -0300

    Ajustando Sidebar

--- config/adminlte.php
+++ config/adminlte.php
@@ -192,9 +192,9 @@ return [
     'classes_content_wrapper' => '',
     'classes_content_header' => '',
     'classes_content' => '',
-    'classes_sidebar' => 'bg-navy sidebar-dark-light elevation-4',
+    'classes_sidebar' => 'sidebar-light-purple elevation-4',
     'classes_sidebar_nav' => '',
-    'classes_topnav' => 'navbar-white navbar-light',
+    'classes_topnav' => 'navbar-light',
     'classes_topnav_nav' => 'navbar-expand',
     'classes_topnav_container' => 'container',
 
